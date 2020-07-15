7393e40f8910f83ddf853f0be33697300e17b52c
Silvio Dias <silviodias.rj@gmail.com>
2020-07-15 03:58:25 -0400

    First Commit

---
+++ aula1/main.js
@@ -0,0 +1,10 @@
+var nome = "Roberto Carlos";
+var idade = 29;
+var idade2 = 30;
+
+var frase = "Gosto muito de correr!";
+alert(nome + " tem "+idade);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toUpperCase());
