604c4b13c28360d4e66ff7b25ff9ac5833272763
Silvio Dias <silviodias.rj@gmail.com>
2020-07-15 05:01:35 -0400

    Conclusao aula 2

--- aula2/main.js
+++ aula2/main.js
@@ -1,6 +1,12 @@
 //criando uma lista
 var lista = ["maçã", "pêra", "uva", "laranja"];
 
+var fruta = { nome: "maçã", cor: "vermelha", formato: "oval"}
+console.log(fruta.nome +" - "+ fruta.cor + " - "+ fruta.formato);
+console.log(fruta);
+
+
+/*
 //adicionando elementos em uma lista
 lista.push("goiaba");
 
@@ -11,3 +17,13 @@ console.log(lista);
 lista.pop();
 
 console.log(lista);
+//length retorna o tamanho da lista
+console.log(lista.length);
+
+//altera a ordem da lista
+console.log(lista.reverse());
+
+console.log(lista.toString());
+
+console.log(lista.join(" - "));
+*/
