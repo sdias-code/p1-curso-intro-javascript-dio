a70ef64f799172e64aead172d35f9bdd3e695b00
Silvio Dias <silviodias.rj@gmail.com>
2020-07-15 04:36:33 -0400

    Aula 02

---
+++ aula2/main.js
@@ -0,0 +1,13 @@
+//criando uma lista
+var lista = ["maçã", "pêra", "uva", "laranja"];
+
+//adicionando elementos em uma lista
+lista.push("goiaba");
+
+//consultando lista
+console.log(lista);
+
+//remove ultimo elemento da lista
+lista.pop();
+
+console.log(lista);
